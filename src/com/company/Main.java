1a64259ac790f906758fdf668793c823d93f3bc9
Vitaliy Belinskiy <vitalybelinsky@gmail.com>
2022-01-21 19:02:35 +0300

    completed part of the homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Human> humans = new ArrayList<>();
+        humans.add(new Human("Jhon1", 15, "1", "Smith1"));
+        humans.add(new Human("Jhon2", 17, "2", "Smith2"));
+        humans.add(new Human("Jhon3", 25, "3", "Smith3"));
+        humans.add(new Human("Jhon4", 31, "4", "Smith4"));
+        humans.add(new Human("Jhon5", 18, "5", "Smith5"));
+        humans.add(new Human("Jhon6", 14, "6", "Smith6"));
+        humans.add(new Human("Jhon7", 49, "7", "Smith7"));
+
+
+
+
+            Map<String, List<String>> map = humans.stream()
+                    .peek(human -> human.setAge(human.getAge() + 1))
+                    .filter(human -> human.getAge() >= 18)
+                    .collect(Collectors.toMap(Human::getId, human -> Arrays.asList(human.getName(),human.getSurname())));
+
+        System.out.println(map);
+        }
+    }
+
+
+
